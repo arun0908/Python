12641e4c5a84e90c6936f907608780aad8d91931
Arun <arunsivaramakrishnan98@gmail.com>
2022-12-01 01:55:30 -0700

    Day 4
    Leet Code Day 4

---
+++ LeetCode/PlusOne.py
@@ -0,0 +1,18 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the
+# integer. The digits are ordered from most significant to the least significant in left-to-right order. The large
+# integer does not contain any leading 0's.
+# Increment the large integer by one and return the resulting array of digits.
+
+def plusOne(digits: list[int]) -> list[int]:
+    for i in range(len(digits)-1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    digits.insert(0, 1)
+    return digits
+
+
+print(plusOne([1, 2, 3]))  # [1,2,3]
+print(plusOne([4, 3, 2, 1]))  # [4,3,2,2]
+print(plusOne([9]))  # [1,0]
