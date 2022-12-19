8b0c58ef30fbbe570927174e890e38ada0d8c1d0
Arun <arunsivaramakrishnan98@gmail.com>
2022-12-18 22:33:51 -0700

    Day 8
    Leet code Day 8

---
+++ LeetCode/CanPlaceFlowers.py
@@ -0,0 +1,16 @@
+# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted
+# in adjacent plots. Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
+# and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers
+# rule.
+
+def canPlaceFlowers(flowerbed: list[int], n: int) -> bool:
+    temp = [0] + flowerbed + [0]
+    for i in range(1, len(temp)-1):
+        if temp[i-1] == 0 and temp[i] == 0 and temp[i+1] == 0:
+            n -= 1
+            temp[i] = 1
+    return n <= 0
+
+
+print(canPlaceFlowers([1,0,0,0,1], 1))  # True
+print(canPlaceFlowers([1,0,0,0,1], 2))  # False
---
+++ LeetCode/MajorityElement.py
@@ -0,0 +1,24 @@
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element
+# always exists in the array.
+
+def majorityElement(nums: list[int]) -> int:
+    # value_map = {}
+    # result, maxCount = 0,0
+    # for i in nums:
+    #     value_map[i] = 1 + value_map.get(i,0)
+    #     result = i if value_map[i] > maxCount else result
+    #     maxCount = max(value_map[i], maxCount)
+    # return result
+
+    # Optimized solution utilizing O(1) space. The algorithm or idea behind this is Boyer-Moore algorithm
+    result, count = 0, 0
+    for i in nums:
+        if count == 0:
+            result = i
+        count += (1 if i == result else -1)
+    return result
+
+
+print(majorityElement([3,2,3]))  # 3
+print(majorityElement([2,2,1,1,1,2,2]))  # 2
