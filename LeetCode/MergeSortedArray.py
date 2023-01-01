fcdcdf713ffc7f8fba607b831167f2943aa794dc
Arun <arunsivaramakrishnan98@gmail.com>
2023-01-01 16:21:59 -0700

    Day 15
    Leet code day 15

--- LeetCode/MergeSortedArray.py
+++ LeetCode/MergeSortedArray.py
@@ -2,17 +2,28 @@ def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
     """
     Do not return anything, modify nums1 in-place instead.
     """
-    # nums1[m:] = nums2[:n]
-    # nums1.sort()
+    # The trick here is to start from the end, as we can fill up the zeros first and then keep the nums1 array same
+
+    # Consider an element which stores the value of the last pointer
+    
+    last = m + n - 1 # Or we can even consider len(nums)-1 as nums1 is of length m + n 
+    # Looping through both arrays while their lengths are greater than 0
     while m > 0 and n > 0:
-        if nums1[m - 1] >= nums2[n - 1]:
-            nums1[m + n - 1] = nums1[m - 1]
+        if nums1[m-1] > nums2[n-1]:
+            nums1[last] = nums1[m-1]
             m -= 1
+        # In case element in nums2 is greater than nums1 or is equal we will replace with element in nums2
         else:
-            nums1[m + n - 1] = nums2[n - 1]
+            nums1[last] = nums2[n-1]
             n -= 1
-    if n > 0:
-        nums1[:n] = nums2[:n]
+        last -= 1
+    # In case elements are left over in m, we can let it be as nums1 is already sorted and in order according to logic
+    # If elements in n are left over, we need to insert them in nums1
+    while n > 0:
+        nums1[last] = nums2[n-1]
+        n, last = n-1, last-1
+    # For de-bugging
+    print(nums1)
 
 
 print(merge([1,2,3,0,0,0],3,[2,5,6],3))  # [1,2,2,3,5,6]
---
+++ LeetCode/ReverseString.py
@@ -0,0 +1,43 @@
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+def reverseString(s: list[str]) -> None:
+    # Using a two pointer approach which is efficient and does the assignment in-place
+    l , r = 0, len(s)-1
+    while l < r:
+        # temp = s[l]
+        # s[l] = s[r]
+        # s[r] = temp
+
+        # Tuple assignment in python eliminates the need to use a temporary variable
+        # In a tuple assignment the lhs is consideres as tuple variables and rhs as tuple values. 
+        # Assigning values takes place simultaneously, so in this case rhs already loads the values and assignings them simultaneously
+        # so if s[l] , s[r] were initially 1 and 2, at assignment it is done as (s[l] , s[r]) = (2 , 1) and 2 is set to l, 1 to r
+        s[l] , s[r] = s[r] , s[l]
+        l , r = l + 1, r - 1
+    
+    # Using a stack approach, but it uses O(n) extra space
+    # temp_stack = []
+    # for c in s:
+    #     temp_stack.append(c)
+
+    # After populating stack, we can use LIFO (last in first out of a stack) to populate original string
+    # i = 0
+    # while temp_stack:
+    #     s[i] = temp_stack.pop()
+    # For de-bugging purposes
+    print(s)
+    
+    # We can use recurssion to solve, by reversiong from out to in, by calling in the function again and again, but reduces speed times
+
+    # Define a function to call recurssively 
+    # def recurrsive(l , r):
+    #     if l < r:
+    #         s[l] , s[r] = s[r] , s[l]
+    #         recurrsive(l + 1, r - 1)
+    # recurrsive(l , r)  # l , r defined in the first step
+    
+
+
+print(reverseString(["h","e","l","l","o"]))  # ['o', 'l', 'l', 'e', 'h']
