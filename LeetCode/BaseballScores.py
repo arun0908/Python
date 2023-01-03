77c559bc04f04ce7de0630ae0b03e9428c9ed8ab
Arun <arunsivaramakrishnan98@gmail.com>
2023-01-03 11:49:44 -0700

    Day 17
    Leet code Day 17

---
+++ LeetCode/BaseballScores.py
@@ -0,0 +1,25 @@
+# You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+# You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+# An integer x: Record a new score of x.
+# '+': Record a new score that is the sum of the previous two scores.
+# 'D': Record a new score that is the double of the previous score.
+# 'C': Invalidate the previous score, removing it from the record.
+# Return the sum of all the scores on the record after applying all the operations.
+
+
+def calPoints(operations: list[str]) -> int:
+    stack = []
+    for o in operations:
+        if o == "+":
+            stack.append(stack[-1] + stack[-2])
+        elif o == "D":
+            stack.append(stack[-1]*2)
+        elif o == "C":
+            stack.pop()
+        else:
+            stack.append(int(o))
+    return sum(stack)
+
+print(calPoints(["5","-2","4","C","D","9","+","+"]))  # 27
+print(calPoints(["5","2","C","D","+"]))  # 30
---
+++ LeetCode/BestTimeToBuyStock.py
@@ -0,0 +1,34 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+def maxProfit(prices: list[int]) -> int:
+    # A more optimized solution using the sliding window concept, where we consider 2 pointers and update them based on the logic.
+    result = 0
+    l , r = 0, 1
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            temp = prices[r] - prices[l]
+            result = max(result, temp)
+        # the trick here would be to update the left pointer, if we find a value lower than our current value, which would,
+        # prevent us from using two loops to keep checking for every possibility.
+        else:
+            l = r
+        r += 1
+    return result
+
+    # Brute Force approach, but does not work always because of time complexity. 
+
+    # for i in range(len(prices)):
+    #     for j in range(i + 1, len(prices)):
+    #         if prices[i]>prices[j]:
+    #             continue
+    #         else:
+    #             temp = prices[j] - prices[i]
+    #             result = max(result, temp)
+    # return result
+
+
+# print(maxProfit([7,1,5,3,6,4]))  # 5 (buy on day 1, sell on day 4)
+# print(maxProfit([7,6,4,3,1]))  # 0
+print(maxProfit([7,3,5,4,1,6])) # 5
