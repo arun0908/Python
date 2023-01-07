394fcb8a2ad1f56b45728d2afd65a4982255c940
Arun <arunsivaramakrishnan98@gmail.com>
2023-01-07 00:43:41 -0700

    Day 19
    Leet Code Day 19

---
+++ LeetCode/SquaresOfSortedArray.py
@@ -0,0 +1,19 @@
+# Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+# Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+# The trick here would be to consider two pointers at the ends and check which is greater and append it and return the reverse of resultant array
+def sortedSquares(nums: list[int]) -> list[int]:
+    result = []
+    l , r = 0, len(nums) - 1
+    while l <= r:
+        if nums[l]*nums[l] > nums[r] * nums[r]:
+            result.append(nums[l]*nums[l])
+            l += 1
+        else:
+            result.append(nums[r]*nums[r])
+            r -= 1
+    return result[::-1]
+
+
+print(sortedSquares([-4,-1,0,3,10]))  # [0,1,9,16,100]
+print(sortedSquares([-7,-3,2,3,11]))  # [4,9,9,49,121]
---
+++ LeetCode/ValidPerfectSquare.py
@@ -0,0 +1,27 @@
+# Given a positive integer num, return true if num is a perfect square or false otherwise.
+# A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+# You must not use any built-in library function, such as sqrt.
+
+def isPerfectSquare(num: int) -> bool:
+# Brute Force approach with O(sqrt(n)) time complexity but not as efficient as binary search
+    # for i in range(1, num+1):
+    #     if i*i == num:
+    #         return True
+    #     if i*i > num:
+    #         return False
+
+# Binary search method, with time complexity of O(log n)
+    l , r = 1, num
+    while l <= r:
+        mid = (l+r)//2
+        if (mid*mid) > num:
+            r = mid-1
+        elif (mid*mid) < num:
+            l = mid + 1
+        else:
+            return True
+    return False
+
+
+print(isPerfectSquare(16))  # True
+print(isPerfectSquare(14))  # False
